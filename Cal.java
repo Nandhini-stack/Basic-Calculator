01b36235ef6b591598da81c52fbd931d4a0000df
Nandhini <84801273+Nandhini-stack@users.noreply.github.com>
2021-08-21 23:56:21 +0530

    Added Code

---
+++ Cal.java
@@ -0,0 +1,111 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+ 
+public class Cal extends Applet
+implements ActionListener
+{
+	String msg=" ";
+	int v1,v2,result;
+	TextField t1;
+	Button b[]=new Button[10];
+	Button add,sub,mul,div,clear,EQ;
+	char OP;
+	public void init()
+	{
+	
+        setBackground(Color.white);
+		t1=new TextField(10);
+		GridLayout gl=new GridLayout(6,6);
+		setLayout(gl);
+		for(int i=0;i<10;i++)
+		{
+			b[i]=new Button(""+i);
+		}
+		add=new Button("add");
+		sub=new Button("sub");
+		mul=new Button("mul");
+		div=new Button("div");
+		clear=new Button("clear");
+		EQ=new Button("EQ");
+		t1.addActionListener(this);
+		add(t1);
+		for(int i=0;i<10;i++)
+		{
+			add(b[i]);
+		}
+		add(add);
+		add(sub);
+		add(mul);
+		add(div);
+		add(clear);
+		add(EQ);
+		for(int i=0;i<10;i++)
+		{
+			b[i].addActionListener(this);
+		}
+		add.addActionListener(this);
+		sub.addActionListener(this);
+		mul.addActionListener(this);
+		div.addActionListener(this);
+		clear.addActionListener(this);
+		EQ.addActionListener(this);
+	}
+ 
+	public void actionPerformed(ActionEvent ae)
+	{
+		String str=ae.getActionCommand();
+		char ch=str.charAt(0);
+		if ( Character.isDigit(ch))
+		t1.setText(t1.getText()+str);
+		else
+		if(str.equals("add"))
+		{
+			v1=Integer.parseInt(t1.getText());
+			OP='+';
+			t1.setText("");
+		}
+		else if(str.equals("sub"))
+		{
+			v1=Integer.parseInt(t1.getText());
+			OP='-';
+			t1.setText("");
+		}
+		else if(str.equals("mul"))
+		{
+			v1=Integer.parseInt(t1.getText());
+			OP='*';
+			t1.setText("");
+		}
+		else if(str.equals("div"))
+		{
+			v1=Integer.parseInt(t1.getText());
+			OP='/';
+			t1.setText("");
+		}
+		else if(str.equals("EQ"))
+		{
+			v2=Integer.parseInt(t1.getText());
+			if(OP=='+')
+				result=v1+v2;
+			else if(OP=='-')
+				result=v1-v2;
+			else if(OP=='*')
+				result=v1*v2;
+			else if(OP=='/')
+				result=v1/v2;
+			t1.setText(""+result);
+		}	
+		if(str.equals("clear"))
+		{
+			t1.setText("");
+		}
+	}
+}
+
+
+/* 
+<applet code="Cal" width=400 height=400>
+</applet>
+*/	
+		
---
+++ JavaCalc.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import java.applet.*;
+public class JavaCalc extends Applet {
+    Label lab;
+    boolean firstDigit = true;
+    float savedValue = 0.0f;     // Initial value
+    String operator = "=";  // Initial operator
+    public void addButtons (Panel p, String labels) {
+        int count = labels.length();
+        for (int i=0;i<count;i++)
+            p.add (new Button (labels.substring(i,i+1)));
+    }
+    public void init () {
+        setLayout (new BorderLayout());
+        add ("North", lab = new Label ("0", Label.RIGHT));
+        Panel p = new Panel();
+        p.setLayout (new GridLayout (4,4));
+        addButtons (p, "789/");
+        addButtons (p, "456*");
+        addButtons (p, "123-");
+        addButtons (p, ".0=+");
+        add ("Center", p);
+    }
+    public boolean action (Event e, Object o) {
+        if (e.target instanceof Button) {
+            String s = (String)o;
+            if ("0123456789.".indexOf (s) != -1) { // isDigit
+                if (firstDigit) {
+                    firstDigit = false;
+                    lab.setText (s);
+                } else {
+                    lab.setText (lab.getText() + s);
+                }
+            } else { // isOperator
+                if (!firstDigit) {
+                    compute (lab.getText());
+                    firstDigit = true;
+                }
+                operator = s;
+            }
+            return true;
+        }
+        return false;
+    }
+    public void compute (String s) {
+        float sValue = new Float (s).floatValue();
+        char c = operator.charAt (0);
+        switch (c) {
+            case '=':   savedValue  = sValue;
+                        break;
+            case '+':   savedValue += sValue;
+                        break;
+            case '-':   savedValue -= sValue;
+                        break;
+            case '*':   savedValue *= sValue;
+                        break;
+            case '/':   savedValue /= sValue;
+                        break;
+        }
+        lab.setText (String.valueOf(savedValue));
+    }
+}
+
+/* 
+<applet code="JavaCalc" width=400 height=400>
+</applet>
+*/	
